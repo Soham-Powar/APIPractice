64312a13db4e50767b9912a5e66b0a918bf9e156
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-05-20 11:14:19 +0530

    Complete code to search gifs by user input.

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 const img = document.querySelector("img");
 const newImgBtn = document.querySelector(".new-img");
 const searchBtn = document.querySelector(".search-btn");
-const input = document.querySelector("#item");
+const searchBar = document.querySelector("#item");
 const form = document.querySelector(".form");
 
-function fetchNewImg() {
-  fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=6laVh6xhrqkllMnPOOQWvozyaWq3K8Xv&s=cats",
-    {
-      mode: "cors",
-    }
-  )
+function fetchNewImg(searchValue = "cats") {
+  fetchURL = `https://api.giphy.com/v1/gifs/translate?api_key=6laVh6xhrqkllMnPOOQWvozyaWq3K8Xv&s=${searchValue}`;
+
+  fetch(fetchURL, {
+    mode: "cors",
+  })
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
-      console.log("done");
       img.src = response.data.images.original.url;
     })
     .catch((e) => {
@@ -27,6 +25,8 @@ document.addEventListener("DOMContentLoaded", () => {
   fetchNewImg();
 });
 
-newImgBtn.addEventListener("click", () => {
-  fetchNewImg();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchValue = searchBar.value;
+  fetchNewImg(searchValue);
 });
